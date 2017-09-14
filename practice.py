e6c0e6d11ff914beeb7274246b5eccd16d1b2c7f
hiye <hiye@spoqa.com>
2017-09-14 18:18:40 +0900

    수정, 삭제 기능 추가

--- practice.py
+++ practice.py
@@ -39,11 +39,26 @@ def teardwon_request(exception):
     g.db.close()
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+    cur = g.db.execute('select title, text, id from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1], id=row[2]) for row in cur.fetchall()]
+
+    result = {'entries': entries}
+
+    #mode = 0:add, 1:edit
+    mode = 0
+
+    if request.method == 'POST':
+        result['origin_title'] = request.form['origin_title']
+        result['origin_text'] = request.form['origin_text']
+        result['id'] = request.form['id']
+        mode = 1
+        flash('EDIT MODE')
+
+    result['mode'] = mode
+
+    return render_template('show_entries.html', result=result)
 
 
 @app.route('/add', methods=['POST'])
@@ -53,6 +68,26 @@ def add_entry():
     g.db.execute('insert into entries (title, text) values(?, ?)', [request.form['title'], request.form['text']])
     g.db.commit()
     flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/edit', methods=['POST'])
+def edit_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('update entries set title = ?, text = ? where id = ?', [request.form['title'], request.form['text'], request.form['id']])
+    g.db.commit()
+    flash('successfully edit no.'+request.form['id'])
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/del', methods=['POST'])
+def del_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('delete from entries where id = ?', [request.form['id']])
+    g.db.commit()
+    flash('successfully delete no.'+request.form['id'])
     return redirect(url_for('show_entries'))
 
 
